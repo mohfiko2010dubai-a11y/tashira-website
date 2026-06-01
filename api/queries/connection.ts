17999f71ec9df947cfc6b5bc9e638d7b0cd5d9e5
Tashira Dev <dev@tashira.me>
2026-06-01 20:46:02 +0800

    fix: add mode: default to drizzle config for MySQL

--- api/queries/connection.ts
+++ api/queries/connection.ts
@@ -10,6 +10,7 @@ let instance: ReturnType<typeof drizzle<typeof fullSchema>>;
 export function getDb() {
   if (!instance) {
     instance = drizzle(env.databaseUrl, {
+      mode: "default",
       schema: fullSchema,
     });
   }
